545aca9f386a4c60ddb72f63caafabca25b02bbd
Sarah Yu <sarah.m.yu88@gmail.com>
2016-05-17 14:07:32 -0700

    Add logic to get zip file, unzip it and send it back as an endpoint to service as a data fallback

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express'),
     app = express(),
     util  = require('util'),
+    path = require('path'),
+    helmet = require('helmet'),
     exec = require('child_process').exec,
     FILE_PATH = process.env.FILE_PATH || '',
     PORT = process.env.PORT || 8000;
+
+app.use(helmet());
 
 app.post('/initialize', function(req, res){
   res.send('working on it');
@@ -32,6 +36,13 @@ app.post('/getResults', function(req, res) {
   });
 });
 
+app.get('/sosResults', function(req, res){
+  
+  res.contentType('application/xml');
+  //won't work unless file exists (must run results.sh to populate)
+  res.sendFile(path.join(__dirname, './sos/X16PP510_0100v7.xml'));
+  
+});
 
 var server = app.listen(PORT, function(){
 
