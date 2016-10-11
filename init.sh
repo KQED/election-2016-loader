82ebe6f9f1f9a3159f03e5412e9e362ce6ff470d
Cynthia Chen <cynthiaechen@gmail.com>
2016-10-11 10:07:14 -0700

    Update scripts with November AP race urls and update table with electWon field.

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ $MYSQL_COMMAND -h $ELECTIONS_DB_HOST --port=$ELECTIONS_DB_PORT --user=$ELECTIONS
 CREATE DATABASE IF NOT EXISTS election2016;
 USE election2016;
 DROP TABLE IF EXISTS APresults;
-CREATE TABLE APresults(officename varchar(30) NOT NULL, seatname varchar(30) NOT NULL, party varchar(25), lastupdated varchar(25) NOT NULL, precincts int(4), firstname varchar(25), lastname varchar(25) NOT NULL, votecount int(10), winner varchar(4), createdAt timestamp NOT NULL default now(), updatedAt DATETIME DEFAULT NULL);
+CREATE TABLE APresults(officename varchar(30) NOT NULL, seatname varchar(30) NOT NULL, party varchar(25), lastupdated varchar(25) NOT NULL, precincts int(4), firstname varchar(25), lastname varchar(25) NOT NULL, votecount int(10), electWon int(10), winner varchar(4), createdAt timestamp NOT NULL default now(), updatedAt DATETIME DEFAULT NULL);
 EOF
