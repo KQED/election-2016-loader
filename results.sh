aadfc106c1279a8273d4a8f71beb3b80b3c10c4d
Cynthia Chen <cynthiaechen@gmail.com>
2016-10-26 14:29:59 -0700

    Update seatname field to allow more characters to fix errors.

--- results.sh
+++ results.sh
@@ -5,7 +5,7 @@ function get_ap_results {
   echo "******************results is running********************"
   query=`echo 'DROP TABLE APresults'`
   echo $query | $MYSQL_COMMAND -h $ELECTIONS_DB_HOST --port=$ELECTIONS_DB_PORT --user=$ELECTIONS_DB_USER --password=$ELECTIONS_DB_PASS election2016
-  query=`echo 'CREATE TABLE APresults(officename varchar(30) NOT NULL, seatname varchar(30) NOT NULL, party varchar(25), lastupdated varchar(25) NOT NULL, precincts int(4), firstname varchar(25), lastname varchar(25) NOT NULL, votecount int(10), electWon int(10), winner varchar(4), createdAt timestamp NOT NULL default now(), updatedAt DATETIME DEFAULT NULL);'`
+  query=`echo 'CREATE TABLE APresults(officename varchar(30) NOT NULL, seatname varchar(50) NOT NULL, party varchar(25), lastupdated varchar(25) NOT NULL, precincts int(4), firstname varchar(25), lastname varchar(25) NOT NULL, votecount int(10), electWon int(10), winner varchar(4), createdAt timestamp NOT NULL default now(), updatedAt DATETIME DEFAULT NULL);'`
   echo $query | $MYSQL_COMMAND -h $ELECTIONS_DB_HOST --port=$ELECTIONS_DB_PORT --user=$ELECTIONS_DB_USER --password=$ELECTIONS_DB_PASS election2016
   #IFS (internal field separator) is reset
   IFS=
